614dd5a1e50c1d2a5b4790487cf46b5787c9ccc0
Spranky <spranky.press@yandex.ru>
2023-03-15 10:51:16 +0300

    Table v1

---
+++ Source/TPS/Game/WorldItemDefault.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/WorldItemDefault.h"
+
+// Sets default values
+AWorldItemDefault::AWorldItemDefault()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWorldItemDefault::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWorldItemDefault::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TPS/Private/Game/ProjectileDefault.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ProjectileDefault.h"
+
+// Sets default values
+AProjectileDefault::AProjectileDefault()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectileDefault::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileDefault::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
