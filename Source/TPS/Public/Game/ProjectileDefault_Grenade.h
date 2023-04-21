14199eb4d5041cc81340ce5699fde4c3887663e4
Spranky <spranky.press@yandex.ru>
2023-04-21 23:13:40 +0300

    7.10

---
+++ Source/TPS/Public/Game/ProjectileDefault_Grenade.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/ProjectileDefault.h"
+#include "ProjectileDefault_Grenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_API AProjectileDefault_Grenade : public AProjectileDefault
+{
+	GENERATED_BODY()
+	
+};
