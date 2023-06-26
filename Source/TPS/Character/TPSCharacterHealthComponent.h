3c3e7973bde89ef9d734c665f5fdcb44ac1f28da
Spranky <spranky.press@yandex.ru>
2023-06-26 23:01:13 +0300

    Health System

---
+++ Source/TPS/Character/TPSCharacterHealthComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TPSHealthComponent.h"
+#include "TPSCharacterHealthComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_API UTPSCharacterHealthComponent : public UTPSHealthComponent
+{
+	GENERATED_BODY()
+	
+};
